50a7ce12d1acd05b1a5e9e6c8ca9238a4aa7af9c
Takazudo <takazudo@gmail.com>
2012-04-08 20:45:05 +0900

    bumped version number to 0.2.3

--- jquery.tinyscroller.js
+++ jquery.tinyscroller.js
@@ -1,4 +1,4 @@
-/*! jQuery.tinyscroller - v0.2.2 -  4/8/2012
+/*! jQuery.tinyscroller - v0.2.3 -  4/8/2012
  * https://github.com/Takazudo/jQuery.tinyscroller
  * Copyright (c) 2012 "Takazudo" Takeshi Takatsudo; Licensed MIT */
 
